e32ef05498edd17b66e9d65c109aea6e6e2243b7
josefarinna <jose.farinna@outlook.com>
2026-09-02 21:41:52 +0200

    [v2] add locale to user browsing settings

--- src/libs/Trakt.NET/Json/Users/TraktUserSettingsBrowsingPost.cs
+++ src/libs/Trakt.NET/Json/Users/TraktUserSettingsBrowsingPost.cs
@@ -3,6 +3,9 @@ namespace TraktNET
     /// <summary>Represents user browsing settings to update.</summary>
     public record class TraktUserSettingsBrowsingPost
     {
+        /// <summary>Gets or sets the user's locale.</summary>
+        public string? Locale { get; set; }
+
         /// <summary>Gets or sets the watch now settings.</summary>
         public TraktUserWatchnowSettings? Watchnow { get; set; }
     }
